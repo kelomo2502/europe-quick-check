74cc761d8fe4d2e26292a43a9cb402e6702f9f88
Gbenga Kelvin Oyewunmi <kelvinoye@gmail.com>
2022-08-11 01:28:58 +0100

    Update App.js
    testing the render interface

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello my Africa Quick Check App</h1>
+      <h1>Hello my Europe Quick Check App</h1>
     </div>
   );
 }
